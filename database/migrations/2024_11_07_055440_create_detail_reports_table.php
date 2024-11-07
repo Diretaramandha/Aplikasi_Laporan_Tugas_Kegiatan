f1ba1d46a53fa8438d0021899b4a435cd79b649a
Direta Ramandha Pratama <diretaramandhapratama@gmail.com>
2024-11-07 13:17:16 +0700

    First Upload

---
+++ database/migrations/2024_11_07_055440_create_detail_reports_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('detail_reports', function (Blueprint $table) {
+            $table->id();
+            $table->text('description');
+            $table->dateTime('datetime');
+            $table->text('link_file');
+            $table->text('file_upload');
+            $table->foreignId('reports_id')->constrained('reports')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('detail_reports');
+    }
+};

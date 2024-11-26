38d2f44d6d89e3186c3ae700162973a6468cbc58
Direta Ramandha Pratama <diretaramandhapratama@gmail.com>
2024-11-27 00:53:52 +0700

    coba progress pa imam

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -12,21 +12,7 @@ use Illuminate\Support\Facades\Log;
 
 class DashboardController extends Controller
 {
-    // public function calculateProgress($subTasks)
-    // {
-    //     $totalSubTasks = $subTasks->count();
-    //     $completedSubTasks = 0;
 
-    //     foreach ($subTasks as $subTask) {
-    //         $completedSubTasks += $subTask->status === 'completed' ? 1 : 0;
-    //         // Jika sub-task memiliki sub-task lain, panggil fungsi ini secara rekursif
-    //         if ($subTask->subTasks->isNotEmpty()) {
-    //             $completedSubTasks += $this->calculateProgress($subTask->subTasks);
-    //         }
-    //     }
-
-    //     return $totalSubTasks > 0 ? ($completedSubTasks / $totalSubTasks) * 100 : 0;
-    // }
     public function view_dashboard()
     {
         $user = Auth::user();
@@ -37,91 +23,89 @@ class DashboardController extends Controller
             ->where('create_by', $user->id)
             ->get();
 
-        // Iterate through the events and calculate the progress for each
-        // foreach ($data['event'] as $event) {
-        //     foreach ($data['event'] as $event) {
-        //         $event->progress = 0; // Initialize $progress
-        //         $event->progress_percentage = 0; // Initialize $progress_percentage
-        //         $event->progress = calculateEventProgress($event);
-        //         $event->progress_percentage = round($event->progress);
-        //     }
-        // }
-
 
         // $data['member'] = Member::all();
         return view("pages.dahsboard.dashboard", $data);
     }
 
 
-    public function view_report_main_tasks($id_event)
+    public function view_report_main_tasks(Request $request, $id_event)
     {
-        // $data['tasks'] = Task::with('event', 'tasks', 'report') // Memuat relasi sub-task
-        //     ->where('id_event', $id_event)
-        //     ->whereNull('tasks_idtask')
+        if ($request->id) {
+            $tasks = Task::where('tasks_idtask',$request->id_tasks)
+            ->where('id_event',$id_event)
+            ->with('event','tasks')
+            ->get();
+        } else {
+            $tasks = Task::whereNull('tasks_idtask')
+            ->where('id_event',$id_event)
+            ->with('event','tasks')
+            ->get();
+        }
+
+        $reports = [];
+
+        foreach ($tasks as $key => $item) {
+            # code...
+            $tasksId = $this->getTasksHierarchy($item->id);
+
+            $tasksIds = collect(json_encode($tasksId))->pluck('id')->toArray();
+
+            $reports[$item->id] = Report::withCount('detailReport')
+            ->whereIn('tasks_idtask',$tasksIds)
+            ->get()
+
+            ->map(function ($report){
+                $report->has_details = $report->detailReport_count > 0 ? 1 : 0;
+                return $report;
+            });
+        }
+
+        // $data['tasks'] = Report::whereHas('tasks', function ($query) use ($id_event) {
+        //     $query->where('id_event', $id_event)
+        //         ->whereNull('tasks_idtask');
+        // })
+        //     ->with(['tasks.event', 'detailReport'])
         //     ->get();
 
-        $data['tasks'] = Report::whereHas('tasks', function ($query) use ($id_event) {
-            $query->where('id_event', $id_event)
-                ->whereNull('tasks_idtask');
-        })
-            ->with(['tasks.event', 'detailReport'])
-            ->get();
+        // // Menghitung progress berdasarkan detail report
+        // foreach ($data['tasks'] as $item) {
+        //     if ($item->detailReport == null) {
+        //         // Jika tidak ada detail report
+        //         $item->progress = 0;
+        //     } else {
+        //         // Jika ada detail report
+        //         $detailReport = $item->detailReport->first(); // Ambil detail report pertama
+        //         if (is_null($detailReport->link_file) && is_null($detailReport->file_upload)) {
+        //             // Jika link_file dan file_upload null
+        //             $item->progress = 50;
+        //         } else {
+        //             // Jika semua sudah diisi
+        //             $item->progress = 100;
+        //         }
+        //     }
 
-        // Menghitung progress berdasarkan detail report
-        foreach ($data['tasks'] as $item) {
-            if ($item->detailReport == null) {
-                // Jika tidak ada detail report
-                $item->progress = 0;
-            } else {
-                // Jika ada detail report
-                $detailReport = $item->detailReport->first(); // Ambil detail report pertama
-                if (is_null($detailReport->link_file) && is_null($detailReport->file_upload)) {
-                    // Jika link_file dan file_upload null
-                    $item->progress = 50;
-                } else {
-                    // Jika semua sudah diisi
-                    $item->progress = 100;
-                }
-            }
-        }
-        // // Menghitung progress untuk setiap main task
-        // foreach ($data['tasks'] as $mainTask) {
-        //     $mainTask->progress = $this->calculateProgress($mainTask);
-        // }
 
-        // echo json_encode($data['tasks']);
-        return view('pages.dahsboard.view_main_report', $data);
+        echo json_encode($tasks);
+        // return view('pages.dahsboard.view_main_report', compact('tasks','reports'));
     }
 
-    // private function calculateProgress($task)
-    // {
-    //     // Ambil semua subtask
-    //     $subTasks = $task->tasks;
-    //     $completedSubtasks = 0;
-    //     $subTaskCount = count($subTasks);
+    function getTasksHierarchy($parent_id){
+        $tasks = Task::select('tasks.id')->where('tasks_idtask',$parent_id)->get();
+        $parent = '[{"id" :'.$parent_id.' }]';
 
-    //     if ($subTaskCount === 0) {
-    //         return 0; // Jika tidak ada subtask, progress = 0
-    //     }
+        $result = collect(json_encode($parent));
 
-    //     foreach ($subTasks as $subTask) {
-    //         // Hitung progress subtask secara rekursif
-    //         $subTaskProgress = $this->calculateProgress($subTask);
+        foreach ($tasks as $key => $task) {
+            # code...
+            $result->push($task);
+            $result = $result->merge($this->getTasksHierarchy($task->id));
+        }
 
-    //         // Jika subtask memiliki detail report, anggap selesai
-    //         if ($subTask->detailReport) {
-    //             $completedSubtasks++;
-    //         }
-    //     }
+        return $result;
+    }
 
-    //     // Hitung rata-rata progres dari semua subtask
-    //     $totalProgress = ($completedSubtasks / $subTaskCount) * 100;
 
-    //     // Jika ada subtask, tambahkan progres dari subtask ke main task
-    //     // Anda juga bisa mempertimbangkan progres dari subtask
-    //     // untuk memperkirakan progres main task jika diinginkan
-    //     return $totalProgress;
-    // }
     public function view_report_tasks($id_event, $id_task)
     {
         $data['task'] = Report::whereHas('tasks', function ($query) use ($id_event, $id_task) {
